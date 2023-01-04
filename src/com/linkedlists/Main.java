eb5d2cba42a376733f92a2cbc956684a5b3e4b47
Saipavanganesh <pavanganeshms@gmail.com>
2023-01-04 14:51:16 +0530

    First Commit

---
+++ src/com/linkedlists/Main.java
@@ -0,0 +1,8 @@
+package com.linkedlists;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Linked Lists");
+        System.out.println("***********************");
+    }
+}
